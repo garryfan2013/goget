b7cb7bb50e82ee9cde49b819c6a61528551bc3e2
fanjia <fan.jia@163.com>
2018-05-06 14:49:02 +0800

    Add commandline flag support

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"path"
 
 	"github.com/garryfan2013/goget/client"
 	"github.com/garryfan2013/goget/config"
@@ -9,15 +11,62 @@ import (
 	"github.com/garryfan2013/goget/record"
 )
 
+/*
+	Example usage:
+	goget -c 5 -o ~/Downloads https://mirrors.tuna.tsinghua.edu.cn/apache/zookeeper/stable/zookeeper-3.4.12.tar.gz
+
+	This will start 5 go routine concurrently, each will deal with the 1/5 of the total file size,
+	the successfully downloaded file will be stored at !/Downloads/zookeeper-3.4.12.tar.gz
+*/
+
 const (
-	FileUrl         = "https://mirrors.tuna.tsinghua.edu.cn/apache/zookeeper/stable/zookeeper-3.4.12.tar.gz"
-	FilePath        = "./zookeeper-3.4.12.tar.gz"
-	ConcurrentCount = "5"
+	Version          = "1.0.0"
+	DefaultTaskCount = 5
 )
 
+var (
+	printHelp bool
+	taskCount int
+	savePath  string
+)
+
+func init() {
+	flag.BoolVar(&printHelp, "h", false, "Printf help messages")
+	flag.IntVar(&taskCount, "c", DefaultTaskCount, "Multi-task count for concurrent downloading")
+	flag.StringVar(&savePath, "o", "./", "The specified download file saved path")
+}
+
+func usage() {
+	fmt.Printf("goget version: %s\n", Version)
+	fmt.Printf("Usage: goget [-h] [-o save_file_path] [-c task_count] remote_url\n\n")
+	fmt.Printf("Options:\n")
+	flag.PrintDefaults()
+}
+
 func main() {
-	var url, path string = FileUrl, FilePath
-	var taskCount string = ConcurrentCount
+	flag.Parse()
+
+	/*
+		Dont allow to have unrecognized flag
+	*/
+	if flag.NArg() > 1 {
+		usage()
+		return
+	}
+
+	var url string
+	if url = flag.Arg(0); url == "" {
+		usage()
+		return
+	}
+
+	/*
+		print help messages
+	*/
+	if printHelp == true {
+		usage()
+		return
+	}
 
 	source, err := client.NewCrawler(client.HttpProtocol)
 	if err != nil {
@@ -44,9 +93,12 @@ func main() {
 		return
 	}
 
+	fileName := path.Base(url)
+	savePath := fmt.Sprintf("%s/%s", savePath, fileName)
+
 	controller.SetConfig(config.KeyRemoteUrl, url)
-	controller.SetConfig(config.KeyLocalPath, path)
-	controller.SetConfig(config.KeyTaskCount, taskCount)
+	controller.SetConfig(config.KeyLocalPath, savePath)
+	controller.SetConfig(config.KeyTaskCount, fmt.Sprintf("%d", taskCount))
 
 	if err = controller.Start(); err != nil {
 		fmt.Println(err)
